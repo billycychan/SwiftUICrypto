746f259704a68cd5e0f1a6323fa0836d4294c5ec
Billy Chan <billycychan.work@gmail.com>
2023-06-22 16:04:19 -0400

    feat: Add a dynamic List to display downloaded coins

--- SwiftUICrypto/Core/Home/Views/HomeView.swift
+++ SwiftUICrypto/Core/Home/Views/HomeView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct HomeView: View {
     
+    @EnvironmentObject private var vm: HomeViewModel
     @State private var showPortfolio: Bool = false
     
     var body: some View {
@@ -20,6 +21,20 @@ struct HomeView: View {
             // content layer
             VStack {
                 homeHeader
+                
+                columnTitles
+                
+                if !showPortfolio {
+                    allCoinsList
+                        .transition(.move(edge: .leading))
+                }
+                
+                if showPortfolio {
+                    portfolioCoinsList
+                        .transition(.move(edge: .trailing))
+                }
+                
+                
                 Spacer(minLength: 0)
             }
         }
@@ -53,6 +68,41 @@ extension HomeView {
         }
         .padding(.horizontal)
     }
+    
+    private var allCoinsList: some View {
+        List {
+            ForEach(vm.allCoins) { coin in
+                CoinRowView(coin: coin, showHoldingColumn: false)
+                    .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
+            }
+        }
+        .listStyle(.plain)
+    }
+    
+    private var portfolioCoinsList: some View {
+        List {
+            ForEach(vm.allCoins) { coin in
+                CoinRowView(coin: coin, showHoldingColumn: true)
+                    .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
+            }
+        }
+        .listStyle(.plain)
+    }
+    
+    private var columnTitles: some View {
+        HStack {
+            Text("Coin")
+            Spacer()
+            if showPortfolio {
+                Text("Holding")
+            }
+            Text("Price")
+                .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
+        }
+        .font(.caption)
+        .foregroundColor(Color.theme.secondaryText)
+        .padding(.horizontal)
+    }
 }
 
 struct HomeView_Previews: PreviewProvider {
@@ -61,5 +111,6 @@ struct HomeView_Previews: PreviewProvider {
             HomeView()
                 .navigationBarHidden(true)
         }
+        .environmentObject(dev.homeVM)
     }
 }
--- SwiftUICrypto/Extensions/PreviewProvider.swift
+++ SwiftUICrypto/Extensions/PreviewProvider.swift
@@ -18,6 +18,8 @@ class DeveloperPreview {
     static let instance = DeveloperPreview()
     private init() {}
     
+    let homeVM = HomeViewModel()
+    
     let coin = CoinModel(
            id: "bitcoin",
            symbol: "btc",
@@ -217,4 +219,5 @@ class DeveloperPreview {
            ]),
            priceChangePercentage24HInCurrency: 3952.64,
            currentHoldings: 1.5)
+    
 }
