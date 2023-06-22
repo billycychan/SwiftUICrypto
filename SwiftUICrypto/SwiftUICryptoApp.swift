caea8007d1077eb95136bcea9d34a6718b3148fb
Billy Chan <billycychan.work@gmail.com>
2023-06-22 12:49:08 -0400

    Initial Commit

---
+++ SwiftUICrypto/SwiftUICryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICryptoApp.swift
+//  SwiftUICrypto
+//
+//  Created by CHI YU CHAN on 22/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
