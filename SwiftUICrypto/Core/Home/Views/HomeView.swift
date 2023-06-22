5c655b6f23be2ea0e10e8ec05c566a4263ab8f52
Billy Chan <billycychan.work@gmail.com>
2023-06-22 13:42:39 -0400

    feat: Creating HomeView and a navigation header

---
+++ SwiftUICrypto/Core/Home/Views/HomeView.swift
@@ -0,0 +1,65 @@
+//
+//  HomeView.swift
+//  SwiftUICrypto
+//
+//  Created by CHI YU CHAN on 22/6/2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @State private var showPortfolio: Bool = false
+    
+    var body: some View {
+        ZStack {
+            // background layer
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            // content layer
+            VStack {
+                homeHeader
+                Spacer(minLength: 0)
+            }
+        }
+    }
+}
+
+extension HomeView {
+    private var homeHeader: some View {
+        HStack {
+            CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                .transaction { transaction in
+                    transaction.animation = nil
+                }
+                .background(
+                    CircleButtonAnimationView(animate: $showPortfolio)
+                )
+            Spacer()
+            Text("Live Prices")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.theme.accent)
+            Spacer()
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                .onTapGesture {
+                    withAnimation(.spring()) {
+                        showPortfolio.toggle()
+                    }
+                }
+
+        }
+        .padding(.horizontal)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            HomeView()
+                .navigationBarHidden(true)
+        }
+    }
+}
