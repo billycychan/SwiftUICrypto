50a21b85913e569e4273857a4ec0564f68796671
Billy Chan <billycychan.work@gmail.com>
2023-06-22 13:00:15 -0400

    feat: Setting up Xcode Project and a custom color theme

--- SwiftUICrypto/ContentView.swift
+++ SwiftUICrypto/ContentView.swift
@@ -9,13 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            VStack(spacing: 40) {
+                Text("AccentColor")
+                    .foregroundColor(Color.theme.accent)
+                Text("Secondary Text Color")
+                    .foregroundColor(Color.theme.secondaryText)
+                Text("Red Color")
+                    .foregroundColor(Color.theme.red)
+                Text("Green color")
+                    .foregroundColor(Color.theme.green)
+            }
         }
-        .padding()
     }
 }
 
---
+++ SwiftUICrypto/Extensions/Color.swift
@@ -0,0 +1,21 @@
+//
+//  Color.swift
+//  SwiftUICrypto
+//
+//  Created by CHI YU CHAN on 22/6/2023.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
